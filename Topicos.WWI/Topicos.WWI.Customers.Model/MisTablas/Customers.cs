849ea685b912edb5bef143d2b81a0f0314e28960
laboratorio <laboratorio@SP-LAB9-PROF>
2018-10-13 16:44:46 -0600

    solución para acceso a BD WWi

---
+++ Topicos.WWI/Topicos.WWI.Customers.Model/MisTablas/Customers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topicos.WWI.Customers.Model
+{
+    public partial class Customers
+    {
+        public int AccountAntiquity
+        {
+            get
+            {
+                int myYears = 0;
+                myYears = (int)((DateTime.Now - AccountOpenedDate).TotalDays / 365.2425);
+                return myYears;
+            }
+            set
+            {
+                ;
+            }
+        }
+
+        public string BillToCustomerName{ get {
+                string result = string.Empty;
+                if (Customers2 != null)
+                    result = Customers2.CustomerName;
+                return result;
+            } set { } }
+
+    }
+}
---
+++ Topicos.WWI/Topicos.WWI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Topicos.WWI.Startup))]
+namespace Topicos.WWI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
